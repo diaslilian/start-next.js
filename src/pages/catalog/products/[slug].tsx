c75cb0d4245692824e2f09abfe67d21d980a396e
diaslilian <lilian.dias25@gmail.com>
2020-10-12 21:34:11 -0300

    first commit

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import { useState } from 'react';
+
+// lazy loading
+const AddToCartModal = dynamic(() => import('../../../components/AddToCartModal'),
+  { loading: () => <p>Loading...</p>, ssr: false }
+)
+
+export default function Product() {
+  const router = useRouter()
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false)
+
+  function handleToCart() {
+    setIsAddToCartModalVisible(true)
+  }
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleToCart}>Add to cart</button>
+
+      {isAddToCartModalVisible && <AddToCartModal />}
+    </div>
+  )
+}
